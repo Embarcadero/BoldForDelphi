2badf39c4fb4354e0bf5d5f4463a8a044db27fde
Roland Bengtsson <roland.bengtsson@aholadigital.com>
2024-12-06 23:29:42 +0200

    Allow VariableHandle w/o system but with type. #20

--- Source/BoldVariableHandle.pas
+++ Source/BoldVariableHandle.pas
@@ -109,10 +109,8 @@ function TBoldVariableHandle.GetValue: TBoldElement;
 begin
   if not (csDesigning in ComponentState) and not (csLoading in ComponentState) then
   begin
-//    if not assigned(StaticSystemHandle) then
-//      raise EBold.CreateFmt(sNoSystemHandle, [classname, 'GetValue', name]); // do not localize
-    if not assigned(StaticBoldType) then
-      raise EBold.CreateFmt(sValueTypeNameInvalid, [classname, name, ValueTypeName]);
+    if not assigned(StaticSystemHandle) and not assigned(StaticBoldType) then
+      raise EBold.CreateFmt(sNoSystemHandle, [classname, 'GetValue', name]); // do not localize
   end;
 
   if {not (csDesigning in ComponentState) and}
